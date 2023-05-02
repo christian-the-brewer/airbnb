d3db33013541071b35f8d3557e9eefa3ce7f069f
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-05-02 15:13:18 +0000

    adds menu

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Container from '../Container'
 import Logo from './Logo'
+import Search from './Search'
+import UserMenu from './UserMenu'
 
 const Navbar = () => {
   return (
@@ -10,6 +12,7 @@ const Navbar = () => {
           <div className='flex flex-row justify-between item-center gap-3 md:gap-0'>
             <Logo />
             <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
