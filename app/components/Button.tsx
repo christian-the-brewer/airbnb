d33c317f02948f2fafa33097e8600924fb118698
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-05-03 14:07:33 +0000

    fix infinite loop error of calling component inside itself

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,26 @@
-const Button = () => {
+'use client'
+
+import { IconType } from 'react-icons'
+
+interface ButtonProps {
+    label: string
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+    disabled?: boolean
+    outline?: boolean
+    small?: boolean
+    icon?: IconType
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    onClick,
+    disabled,
+    outline,
+    small,
+    icon
+}) => {
     return (
-        <Button></Button>
+        <button></button>
     )
 }
 
